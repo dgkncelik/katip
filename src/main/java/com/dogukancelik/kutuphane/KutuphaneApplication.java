dcb7e58ab8dfa34ba6e78325505a1acfaa5d8318
Doğukan ÇELİK <dogukan.celik@itu.edu.tr>
2018-05-24 00:11:39 +0300

    Service layer implemented in creation pattern with repositories.

---
+++ src/main/java/com/dogukancelik/kutuphane/KutuphaneApplication.java
@@ -0,0 +1,12 @@
+package com.dogukancelik.kutuphane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KutuphaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KutuphaneApplication.class, args);
+    }
+}
